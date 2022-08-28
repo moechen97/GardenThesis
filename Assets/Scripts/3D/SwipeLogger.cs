ef490562e283f0e570e91f9a2cc308637c5275d7
Ryan Frazier <ryanfraz@usc.edu>
2022-08-28 13:02:28 -0700

    add grid and flowers

---
+++ Assets/Scripts/3D/SwipeLogger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeLogger : MonoBehaviour
+{
+    private void Awake()
+    {
+        SwipeDetector.OnSwipe += SwipeDetector_OnSwipe;
+    }
+
+    private void SwipeDetector_OnSwipe(SwipeDetector.SwipeData data)
+    {
+        Debug.Log("Swipe in Direction: " + data.Direction);
+    }
+}
