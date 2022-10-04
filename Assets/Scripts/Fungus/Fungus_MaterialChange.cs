67558cabbbedd3894d8f1648c3da8cb792fd263d
Yimeng Chen <69623061+moechen97@users.noreply.github.com>
2022-10-04 14:35:41 -0700

    Add glow function to Fungus

--- Assets/Scripts/Fungus/Fungus_MaterialChange.cs
+++ Assets/Scripts/Fungus/Fungus_MaterialChange.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Fungus_MaterialChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Renderer fungusRenderer;
+    [SerializeField] private float changeSpeed = 1f;
+    [SerializeField] private float glowExtentMax;
+    [SerializeField] private float witheredExtent;
+    [SerializeField] private Animator fungusAnimator;
+
+    private void OnEnable()
     {
-        
+        TimeEventManager.NightStart += GotoNight;
+        TimeEventManager.DayStart += GotoDay;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        TimeEventManager.NightStart -= GotoNight;
+        TimeEventManager.DayStart -= GotoDay;
+    }
+
+    void GotoNight()
+    {
+        fungusRenderer.material.DOFloat(glowExtentMax,"_GlowExtent",changeSpeed);
+    }
+
+    void GotoDay()
+    {
+        fungusRenderer.material.DOFloat(0,"_GlowExtent",changeSpeed);
+    }
+
+    void Withered()
+    {
+        fungusAnimator.SetBool("isWithered",true);
     }
 }
--- Assets/Scripts/Sky/TimeEventManager.cs
+++ Assets/Scripts/Sky/TimeEventManager.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 public class TimeEventManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float dayTimeStart;
+    [SerializeField] private float duskTimeStart;
+    [SerializeField] private float nightTimeStart;
+    //state = 0 = night , state = 1 = day , state = 2 = dusk
+    private int state = 0;
+    private float currentTime;
+
+    public delegate void TimePeriod();
+    public static event TimePeriod DayStart;
+    public static event TimePeriod DuskStart;
+    public static event TimePeriod NightStart;
+
     void Start()
     {
-        
+        currentTime = ProceduralSky.TimeofDay;
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentTime = ProceduralSky.TimeofDay;
         
+        if (currentTime > dayTimeStart && state == 0 && currentTime<=duskTimeStart)
+        {
+            if (DayStart != null)
+            {
+                
+                DayStart();
+            }
+            Debug.Log("Day");
+            state = 1;
+        }
+        else if (currentTime > duskTimeStart && state == 1 && currentTime<=nightTimeStart)
+        {
+            if (DuskStart != null)
+            {
+                DuskStart();
+            }
+            Debug.Log("Dusk");
+            state = 2;
+        }
+        else if (currentTime > nightTimeStart && state == 2)
+        {
+            if (NightStart != null)
+            {
+                NightStart();
+            }
+            Debug.Log("Night");
+            state = 0;
+        }
     }
 }
