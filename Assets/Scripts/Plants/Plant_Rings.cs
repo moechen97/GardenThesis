071891239f1b5913de4ba8dfe881cb2e30aaaec3
Yimeng Chen <69623061+moechen97@users.noreply.github.com>
2022-11-16 02:17:30 -0800

    add lotus rings

---
+++ Assets/Scripts/Plants/Plant_Rings.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Planting
+{
+    public class Plant_Rings : Plant
+    {
+        [SerializeField, Tooltip("Minimum height")] float height_minRange = 0.6F;
+        [SerializeField, Tooltip("Maximum height")] float height_maxRange = 1.0F;
+        [SerializeField, Tooltip("X/Y Scale")] float XZScale = 1F;
+        protected override void Start()
+        {
+            float randomRotateY = Random.Range(0f, 360f);
+            transform.rotation = Quaternion.Euler(new Vector3(0,randomRotateY,0));
+            transform.localScale = new Vector3(XZScale, Random.Range(height_minRange, height_maxRange), XZScale);
+            id = PlantType.Plant_Drum;
+            finishGrowAnimationName = "Plant_Rings_FullyGrow";
+            base.Start(); 
+            //Debug.Log("Mushroom Dark Green: " + PlantManager.plantCounter[id]);
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+    }
+
+}
--- Assets/Scripts/Plants/UnlockablePlants.cs
+++ Assets/Scripts/Plants/UnlockablePlants.cs
@@ -98,6 +98,30 @@ namespace Planting {
                         unlocks.Add(PlantType.Plant_Capture);
                     }
                 }
+                if(unlockable.Key == PlantType.Plant_Rings)
+                {
+                    if(PlantManager.plantedPlantCounter[PlantType.Plant_Peach] >= 5 && 
+                       PlantManager.bredPlantCounter[PlantType.Plant_Peach] >= 2)
+                    {
+                        GameObject fungusPurpleIcon = GameObject.Instantiate(unlockable_icons[unlockable.Key]);
+                        fungusPurpleIcon.name = PlantType.Plant_Rings.ToString();
+                        fungusPurpleIcon.transform.parent = seedPanel.transform;
+                        fungusPurpleIcon.transform.localScale = new Vector3(1.440003F, 1.440003F, 1.440003F);
+                        unlocks.Add(PlantType.Plant_Rings);
+                    }
+                }
+                if(unlockable.Key == PlantType.Plant_Lotus)
+                {
+                    if(PlantManager.plantedPlantCounter[PlantType.Fungus_Green] >= 1 && 
+                       PlantManager.bredPlantCounter[PlantType.Fungus_Green] >= 0)
+                    {
+                        GameObject fungusPurpleIcon = GameObject.Instantiate(unlockable_icons[unlockable.Key]);
+                        fungusPurpleIcon.name = PlantType.Plant_Lotus.ToString();
+                        fungusPurpleIcon.transform.parent = seedPanel.transform;
+                        fungusPurpleIcon.transform.localScale = new Vector3(1.440003F, 1.440003F, 1.440003F);
+                        unlocks.Add(PlantType.Plant_Lotus);
+                    }
+                }
             }
             foreach(PlantType type in unlocks)
             {
