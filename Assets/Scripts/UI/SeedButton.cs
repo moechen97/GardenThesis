610fe12b343f22a229b2b9057af79f2bda2717dc
Yimeng Chen <69623061+moechen97@users.noreply.github.com>
2022-11-28 02:03:13 -0800

    Add instruction finger

--- Assets/Scripts/UI/SeedButton.cs
+++ Assets/Scripts/UI/SeedButton.cs
@@ -1,29 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SeedButton : MonoBehaviour
+namespace Planting
 {
-    [SerializeField] private Animator uiAnimator;
-    [SerializeField] private Animator Seedbar;
-    private bool isclosed = false;
-    private bool canInteract = true;
-
-    public void TapButton()
+    public class SeedButton : MonoBehaviour
     {
-        if (!canInteract)
-            return;
-        isclosed = !isclosed;
-        canInteract = false;
-        uiAnimator.SetBool("IsClosed",isclosed);
-        Seedbar.SetBool("IsShown",isclosed);
-        StartCoroutine(Colddown());
-    }
+        [SerializeField] private Animator uiAnimator;
+        [SerializeField] private Animator Seedbar;
+        [SerializeField] private GameObject FingerHint;
+        private bool isclosed = false;
+        private bool canInteract = true;
+        private bool hasPlanted = false;
 
-    IEnumerator Colddown()
-    {
-        yield return new WaitForSeconds(0.2f);
-        canInteract = true;
+        public void TapButton()
+        {
+            if (!canInteract)
+                return;
+            isclosed = !isclosed;
+            canInteract = false;
+            uiAnimator.SetBool("IsClosed",isclosed);
+            Seedbar.SetBool("IsShown",isclosed);
+            if (isclosed && !hasPlanted)
+            {
+                StartCoroutine(SetFingerHintActive());
+            }
+            else if (!isclosed && !hasPlanted)
+            {
+                StopCoroutine(SetFingerHintActive());
+                FingerHint.SetActive(false);
+            }
+            StartCoroutine(Colddown());
+        }
+
+        IEnumerator Colddown()
+        {
+            yield return new WaitForSeconds(0.5f);
+            canInteract = true;
+        }
+
+        IEnumerator SetFingerHintActive()
+        {
+            yield return new WaitForSeconds(1f);
+            FingerHint.SetActive(true);
+        }
+        
+        private void Update()
+        {
+            if(hasPlanted)
+                return;
+            float  resourceUsed = Resources.GetResourcesUsed();
+            Debug.Log("resourceUsed:" + resourceUsed);
+            if (resourceUsed > 0)
+            {
+                hasPlanted = true;
+                FingerHint.SetActive(false);
+            }
+        }
     }
 
 }
